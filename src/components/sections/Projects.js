f044f6b630c28cc336aa1e4e5e9ba17c1b53f9d4
Brandon <BmcPrimary@yahoo.com>
2019-03-25 19:09:30 -0400

    Added slider to projects section

--- src/components/sections/Projects.js
+++ src/components/sections/Projects.js
@@ -1,43 +1,81 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Slider from 'react-slick';
+
 import styled from 'styled-components';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 import { Universal__Padding, Section__Padding } from '../mixins';
 
 const ProjectContainer = styled.section`
   ${Universal__Padding}
   ${Section__Padding}
+
+  .slider {
+    img {
+      float: right;
+    }
+  }
+
+  .slick-prev:before,
+  .slick-next:before {
+    color: #000000;
+  }
+
+  .slick-prev {
+    left: 1% !important;
+    z-index: 1;
+  }
+  .slick-next {
+    right: 1% !important;
+    z-index: 1;
+  }
 `;
 
-const Projects = () => {
-  return (
-    <ProjectContainer>
-      <h1>My Projects</h1>
-      <p>
-        Lorem ipsum dolor sit amet, ea nec fugit essent officiis. Malorum
-        facilisis cum ea, cum ad vitae suscipit, per omnium probatus sapientem
-        ei. Mutat animal facilis ex nec, id reprehendunt delicatissimi sea, sed
-        mundi antiopam ut. Alia adhuc facete has an, sea modo consetetur id. Quo
-        cu ignota accumsan. Vel et modo doming laoreet, has et meis ornatus, in
-        noster commune splendide has. Et quo ancillae lucilius scriptorem, per
-        feugait expetenda at. Ullum labores ea eam, cu posse oblique
-        definitionem nec. Ne pri vide repudiare definitionem, mel an nobis
-        abhorreant. Nihil interpretaris ne nam, denique mandamus ad pro, usu ad
-        oportere similique. Choro fuisset eu sed, et mundi insolens honestatis
-        quo. Ridens tibique vim ne, te assum vivendum deseruisse eum, mei
-        vidisse scripta cu. Quas aperiri nominavi ea qui, ut sea etiam iisque.
-        Inimicus scribentur et pri, ei eos malis praesent concludaturque. Pro
-        mollis cetero ut. Duis appellantur reprehendunt vis cu, omnes qualisque
-        at sit. An qui electram partiendo adipiscing. Sed eu dico possit
-        ceteros, ea vix brute etiam assueverit. Eos essent volutpat ei, nam
-        veniam doctus no. Sit prompta placerat ea, ius inani tractatos cu. Mazim
-        ridens sed in. Nec summo ludus scripta id. Nam nonumy docendi at, justo
-        discere usu et. Ei accumsan honestatis pri, cu rebum autem usu, cu sed
-        quis eligendi. Regione eloquentiam in duo, luptatum perfecto nam ne,
-        diam imperdiet vis no. His at verear euismod. Habeo nominati ocurreret
-        has ea. Causae laoreet quo e
-      </p>
-    </ProjectContainer>
-  );
-};
+class Projects extends Component {
+  render() {
+    const settings = {
+      arrows: true,
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      className: 'slider'
+    };
+
+    return (
+      <ProjectContainer>
+        <h1>Projects</h1>
+        <Slider {...settings}>
+          <div>
+            <h3>1</h3>
+            <img src="https://placeimg.com/640/480/any" />
+          </div>
+          <div>
+            <h3>2</h3>
+            <img src="https://placeimg.com/640/480/any" />
+          </div>
+          <div>
+            <h3>3</h3>
+            <img src="https://placeimg.com/640/480/any" />
+          </div>
+          <div>
+            <h3>4</h3>
+            <img src="https://placeimg.com/640/480/any" />
+          </div>
+          <div>
+            <h3>5</h3>
+            <img src="https://placeimg.com/640/480/any" />
+          </div>
+          <div>
+            <h3>6</h3>
+            <img src="https://placeimg.com/640/480/any" />
+          </div>
+        </Slider>
+      </ProjectContainer>
+    );
+  }
+}
 
 export default Projects;
