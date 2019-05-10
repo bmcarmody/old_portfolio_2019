52e623449cdd4f77836a38fd2f4fd91048aed492
Brandon <BmcPrimary@yahoo.com>
2019-05-10 13:36:52 -0400

    Netlify form test

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -7,65 +7,64 @@ const ContactContainer = styled.section`
   ${Universal__Padding}
   ${Section__Padding}
   background-color: rgba(245,245,245, .5);
+
+  form {
+    input {
+      width: 50%;
+      height: 3rem;
+      padding-left: 1rem;
+      border-radius: 0.5rem;
+      border: 1px solid #808080;
+      font-size: 1.5rem;
+      margin-bottom: 2.5rem;
+
+      &:focus {
+        outline: none;
+        border: 1px solid rgba(241, 92, 92, 0.8);
+      }
+
+      @media screen and (max-width: 1024px) {
+        width: 100%;
+      }
+    }
+
+    textarea {
+      width: 50%;
+      height: 12.5rem;
+      padding-top: 1rem;
+      padding-left: 1rem;
+      border-radius: 0.5rem;
+      border: 1px solid #808080;
+      font-size: 1.5rem;
+      margin-bottom: 2.5rem;
+
+      &:focus {
+        outline: none;
+        border: 1px solid rgba(241, 92, 92, 0.8);
+      }
+
+      @media screen and (max-width: 1024px) {
+        width: 100%;
+      }
+    }
+
+    .submit {
+      border: none;
+      height: 3.5rem;
+      padding: 0 1rem;
+      width: fit-content;
+
+      @media screen and (max-width: 1024px) {
+        width: 100%;
+      }
+    }
+  }
 `;
-
-const FormContainer = styled.form`
-  input {
-    width: 50%;
-    height: 3rem;
-    padding-left: 1rem;
-    border-radius: 0.5rem;
-    border: 1px solid #808080;
-    font-size: 1.5rem;
-    margin-bottom: 2.5rem;
-
-    &:focus {
-      outline: none;
-      border: 1px solid rgba(241, 92, 92, 0.8);
-    }
-
-    @media screen and (max-width: 1024px) {
-      width: 100%;
-    }
-  }
-
-  textarea {
-    width: 50%;
-    height: 12.5rem;
-    padding-top: 1rem;
-    padding-left: 1rem;
-    border-radius: 0.5rem;
-    border: 1px solid #808080;
-    font-size: 1.5rem;
-    margin-bottom: 2.5rem;
-
-    &:focus {
-      outline: none;
-      border: 1px solid rgba(241, 92, 92, 0.8);
-    }
-
-    @media screen and (max-width: 1024px) {
-      width: 100%;
-    }
-  }
-
-  .submit {
-    border: none;
-    height: 3.5rem;
-    padding: 0 1rem;
-    width: fit-content;
-
-    @media screen and (max-width: 1024px) {
-      width: 100%;
-    }
-  }
-`;
-
 const Contact = () => {
   return (
     <ContactContainer id="contact">
       <h1>Contact</h1>
-      <FormContainer
+      <form
         name="contact"
         action="POST"
         data-netlify="true"
@@ -74,14 +73,14 @@ const Contact = () => {
         <input type="text" placeholder="Name" name="name" required />
         <br />
 
-        <input type="text" placeholder="Email" name="email" required />
+        <input type="email" placeholder="Email" name="email" required />
         <br />
 
         <textarea type="text" placeholder="Message" name="text" required />
         <br />
         <div data-netlify-recaptcha="true" />
         <input type="submit" className="submit btn" />
-      </FormContainer>
+      </form>
     </ContactContainer>
   );
 };
