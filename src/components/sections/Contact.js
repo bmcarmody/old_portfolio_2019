8439fd54cd6621fb42f2fb2bed22ee91391b6499
Brandon <BmcPrimary@yahoo.com>
2019-05-10 23:11:04 -0400

    Moved form above root;

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -65,20 +65,26 @@ const Contact = () => {
     <ContactContainer id="contact">
       <h1>Contact</h1>
       <form name="contact" method="post">
-          <input type="hidden" name="form-name" value="contact" />
-          <p>
-            <label>Your Name: <input type="text" name="name"/></label>
-          </p>
-          <p>
-            <label>Your Email: <input type="email" name="email"/></label>
-          </p>
-          <p>
-            <label>Message: <textarea name="message"></textarea></label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
+        <input type="hidden" name="form-name" value="contact" />
+        <p>
+          <label>
+            Your Name: <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email: <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name="message" />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
     </ContactContainer>
   );
 };
